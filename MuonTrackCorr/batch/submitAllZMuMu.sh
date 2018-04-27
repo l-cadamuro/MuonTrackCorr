c95c8274cbb423b776faa84984d42d090ebbddde
Luca Cadamuro <lc.cadamuro@gmail.com>
2018-04-27 07:29:53 -0500

    submit ZMuMu

---
+++ MuonTrackCorr/batch/submitAllZMuMu.sh
@@ -0,0 +1,8 @@
+SUFFIX_TAG="27Apr_TkMu"
+
+echo "... preparing the submission (tar and copy of CMSSW)"
+python submitOnTier3.py --xrdcp-tar-only
+echo ".. done, launching"
+
+python submitOnTier3.py --filelist filelist/ZToMuMu_RelVal_PU0.txt     --tag ZToMuMu_RelVal_PU0_${SUFFIX_TAG}   --njobs 50 --no-tar --no-xrdcp-tar
+python submitOnTier3.py --filelist filelist/ZToMuMu_RelVal_PU200.txt   --tag ZToMuMu_RelVal_PU200_${SUFFIX_TAG} --njobs 50 --no-tar --no-xrdcp-tar
