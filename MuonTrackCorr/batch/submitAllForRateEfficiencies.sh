37be33249d550babbe43e48b1c6ab755d4f96bc0
Luca Cadamuro <lc.cadamuro@gmail.com>
2018-10-12 18:38:27 -0500

    update list to run on

--- MuonTrackCorr/batch/submitAllForRateEfficiencies.sh
+++ MuonTrackCorr/batch/submitAllForRateEfficiencies.sh
@@ -6,10 +6,10 @@ echo ".. done, launching"
 
 ### single mu eff
 python submitOnTier3.py --filelist filelist/SingleMu_FlatPt-2to100_L1TnoPU.txt          --tag SingleMu_FlatPt-2to100_L1TnoPU_${SUFFIX_TAG}     --njobs 50 --no-tar --no-xrdcp-tar
-python submitOnTier3.py --filelist filelist/SingleMu_FlatPt-2to100_L1TPU140.txt         --tag SingleMu_FlatPt-2to100_L1TPU140_${SUFFIX_TAG}    --njobs 50 --no-tar --no-xrdcp-tar
+#python submitOnTier3.py --filelist filelist/SingleMu_FlatPt-2to100_L1TPU140.txt         --tag SingleMu_FlatPt-2to100_L1TPU140_${SUFFIX_TAG}    --njobs 50 --no-tar --no-xrdcp-tar
 python submitOnTier3.py --filelist filelist/SingleMu_FlatPt-2to100_L1TPU200.txt         --tag SingleMu_FlatPt-2to100_L1TPU200_${SUFFIX_TAG}    --njobs 50 --no-tar --no-xrdcp-tar
 
 ### rates
-python submitOnTier3.py --filelist filelist/SingleNeutrino_L1TnoPU.txt          --tag SingleNeutrino_L1TnoPU_${SUFFIX_TAG}     --njobs 200 --no-tar --no-xrdcp-tar
+#python submitOnTier3.py --filelist filelist/SingleNeutrino_L1TnoPU.txt          --tag SingleNeutrino_L1TnoPU_${SUFFIX_TAG}     --njobs 200 --no-tar --no-xrdcp-tar
 python submitOnTier3.py --filelist filelist/SingleNeutrino_L1TPU140.txt         --tag SingleNeutrino_L1TPU140_${SUFFIX_TAG}    --njobs 200 --no-tar --no-xrdcp-tar
 python submitOnTier3.py --filelist filelist/SingleNeutrino_L1TPU200.txt         --tag SingleNeutrino_L1TPU200_${SUFFIX_TAG}    --njobs 200 --no-tar --no-xrdcp-tar
