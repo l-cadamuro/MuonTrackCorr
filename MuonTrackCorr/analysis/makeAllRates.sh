b1bb9c2c937161ad1290b50ee694f7efdef0a571
Luca Cadamuro <lc.cadamuro@gmail.com>
2019-10-01 04:18:05 -0500

    code for TDR rates

---
+++ MuonTrackCorr/analysis/makeAllRates.sh
@@ -0,0 +1,3 @@
+./makeRate filelist/TDR_MC_EMTFpp/NuGun_140PU.txt rate_EMTFpp_140PU.root
+./makeRate filelist/TDR_MC_EMTFpp/NuGun_200PU.txt rate_EMTFpp_200PU.root
+./makeRate filelist/TDR_MC_EMTFpp/NuGun_250PU.txt rate_EMTFpp_250PU.root
--- MuonTrackCorr/analysis/makeRate.cpp
+++ MuonTrackCorr/analysis/makeRate.cpp
@@ -79,7 +79,7 @@ int main(int argc, char** argv)
     float sftor  = 0.5;
     float sftor_initial = 0.0;
 
-    // int maxEvts = 100000;
+    // int maxEvts = 10000;
     int maxEvts = -1;
 
     /// -----------------------------------------------------------
@@ -95,13 +95,23 @@ int main(int argc, char** argv)
 
     RateCalculator rc_EMTF("EMTF");
     RateCalculator rc_TPTkMu("TPTkMu");
+    RateCalculator rc_TPTkMu_truemu("TPTkMu_truemu");
+    RateCalculator rc_TPTkMu_fakemu("TPTkMu_fakemu");
     RateCalculator rc_MyTPTkMu("MyTPTkMu"); // my implementation
     RateCalculator rc_UpgTkMu("UpgTkMu");
+    RateCalculator rc_TPTkMuStub("TPTkMuStub");
+    RateCalculator rc_TPTkMuStub_truemu("TPTkMuStub_truemu");
+    RateCalculator rc_TPTkMuStub_fakemu("TPTkMuStub_fakemu");
 
     rc_EMTF.setStyle(kBlue);
     rc_TPTkMu.setStyle(kRed);
+    rc_TPTkMu_truemu.setStyle(kRed+2);
+    rc_TPTkMu_fakemu.setStyle(kRed-9);
     rc_MyTPTkMu.setStyle(kOrange);
     rc_UpgTkMu.setStyle(kGreen+1);
+    rc_TPTkMuStub.setStyle(kGreen);
+    rc_TPTkMuStub_truemu.setStyle(kGreen+2);
+    rc_TPTkMuStub_fakemu.setStyle(kGreen-9);
 
     TFile* fOut = new TFile (outputname.c_str(), "recreate");
 
@@ -165,9 +175,35 @@ int main(int argc, char** argv)
             float aeta = std::fabs(mtkt.L1_TkMu_eta.At(itkmu));
             if (aeta < fiducial_eta_min || aeta > fiducial_eta_max)
                 continue;
+            if (mtkt.L1_TkMu_mudetID.At(itkmu) != 3)
+                continue; // just the ManTra endcap rate
+
             // cout << mtkt.L1_TkMu_eta.At(itkmu) << endl;
             rc_TPTkMu.feedPt(mtkt.L1_TkMu_pt.At(itkmu));
+            int genmatchedTP = mtkt.L1_TkMu_gen_TP_ID.At(itkmu);
+            if (abs(genmatchedTP) == 13)
+                rc_TPTkMu_truemu.feedPt(mtkt.L1_TkMu_pt.At(itkmu));
+            else
+                rc_TPTkMu_fakemu.feedPt(mtkt.L1_TkMu_pt.At(itkmu));
+
         }
+
+        for (uint itkmustub = 0; itkmustub < *(mtkt.n_L1_TkMuStub); ++itkmustub){
+            float aeta = std::fabs(mtkt.L1_TkMuStub_eta.At(itkmustub));
+            if (aeta < fiducial_eta_min || aeta > fiducial_eta_max)
+                continue;
+            // if (mtkt.L1_TkMu_mudetID.At(itkmustub) != 3)
+            //     continue; // TkMuSTub only for EMTF hits
+
+            // cout << mtkt.L1_TkMu_eta.At(itkmustub) << endl;
+            rc_TPTkMuStub.feedPt(mtkt.L1_TkMuStub_pt.At(itkmustub));
+            int genmatchedTP = mtkt.L1_TkMuStub_gen_TP_ID.At(itkmustub);
+            if (abs(genmatchedTP) == 13)
+                rc_TPTkMuStub_truemu.feedPt(mtkt.L1_TkMuStub_pt.At(itkmustub));
+            else
+                rc_TPTkMuStub_fakemu.feedPt(mtkt.L1_TkMuStub_pt.At(itkmustub));
+        }
+
 
         // upg correlator
         auto corr_mu_idxs = corr.find_match(mtkt); // for each trk, the emtfmu that matches
@@ -191,19 +227,34 @@ int main(int argc, char** argv)
         // make the rate plots here, once all pt filled
         rc_EMTF.process();
         rc_TPTkMu.process();
+        rc_TPTkMu_truemu.process();
+        rc_TPTkMu_fakemu.process();
         rc_MyTPTkMu.process();
         rc_UpgTkMu.process();
+        rc_TPTkMuStub.process();
+        rc_TPTkMuStub_truemu.process();
+        rc_TPTkMuStub_fakemu.process();
     }
 
     // make rate plots
     rc_EMTF.makeRatePlot();
     rc_TPTkMu.makeRatePlot();
+    rc_TPTkMu_truemu.makeRatePlot();
+    rc_TPTkMu_fakemu.makeRatePlot();
     rc_MyTPTkMu.makeRatePlot();
     rc_UpgTkMu.makeRatePlot();
+    rc_TPTkMuStub.makeRatePlot();
+    rc_TPTkMuStub_truemu.makeRatePlot();
+    rc_TPTkMuStub_fakemu.makeRatePlot();
 
     // save to file
     rc_EMTF.saveToFile(fOut);
     rc_TPTkMu.saveToFile(fOut);
+    rc_TPTkMu_truemu.saveToFile(fOut);
+    rc_TPTkMu_fakemu.saveToFile(fOut);
     rc_MyTPTkMu.saveToFile(fOut);
     rc_UpgTkMu.saveToFile(fOut);
+    rc_TPTkMuStub.saveToFile(fOut);
+    rc_TPTkMuStub_truemu.saveToFile(fOut);
+    rc_TPTkMuStub_fakemu.saveToFile(fOut);
 }
